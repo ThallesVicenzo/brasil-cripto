c47b302ad2109bb83cb2cd022af5b213605dc91d
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2025-07-09 20:33:49 -0300

    feat: add UiOverlayColor widget for customizable overlay styles

---
+++ lib/view/utils/widgets/colors/ui_overlay_color.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class UiOverlayColor extends StatelessWidget {
+  const UiOverlayColor({
+    super.key,
+    required this.child,
+    this.overlayColor,
+    this.brightness,
+  });
+
+  final Widget child;
+  final Color? overlayColor;
+  final Brightness? brightness;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarColor: overlayColor ?? Colors.black87,
+        statusBarIconBrightness: brightness ?? Brightness.light,
+      ),
+      child: child,
+    );
+  }
+}
