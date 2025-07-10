63ecda1b16bf9088950cb4d72fa0514ddba3feaa
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2025-07-10 14:28:17 -0300

    feat: add localization support with English and Portuguese translations

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,7 @@
+import 'package:brasil_cripto/app_injector.dart';
+import 'package:brasil_cripto/l10n/global_app_localizations.dart';
 import 'package:brasil_cripto/view/utils/routes/manager/app_main_routes_manager.dart';
+import 'package:brasil_cripto/view/utils/widgets/animations/falling_coins_background.dart';
 import 'package:brasil_cripto/view/utils/widgets/colors/ui_overlay_color.dart';
 import 'package:flutter/material.dart';
 
@@ -11,8 +14,14 @@ class BrasilCripto extends StatelessWidget {
       title: 'Brasil Cripto',
       debugShowCheckedModeBanner: false,
       routerConfig: router,
-      theme: ThemeData(scaffoldBackgroundColor: Colors.black87),
+      theme: ThemeData(scaffoldBackgroundColor: Colors.transparent),
+      supportedLocales: AppLocalizations.supportedLocales,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
       builder: (context, child) {
+        sl<GlobalAppLocalizations>().setAppLocalizations(
+          AppLocalizations.of(context),
+        );
+
         return GestureDetector(
           onTap: () {
             ScaffoldMessenger.of(
@@ -20,7 +29,15 @@ class BrasilCripto extends StatelessWidget {
             ).removeCurrentSnackBar(reason: SnackBarClosedReason.swipe);
             FocusManager.instance.primaryFocus?.unfocus();
           },
-          child: UiOverlayColor(child: child!),
+          child: FallingCoinsBackground(
+            numberOfCoins: 20,
+            child: UiOverlayColor(
+              child: Padding(
+                padding: EdgeInsets.only(bottom: 40),
+                child: child!,
+              ),
+            ),
+          ),
         );
       },
     );
