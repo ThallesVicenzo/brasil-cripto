9d2ef1ba4c6b1f58edd057588cab3577153ea034
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2025-07-11 18:12:14 -0300

    feat: update WelcomePage layout with DefaultPadding and localizations integration

--- lib/view/pages/welcome_page.dart
+++ lib/view/pages/welcome_page.dart
@@ -1,13 +1,15 @@
 import 'dart:async';
 
+import 'package:brasil_cripto/app_injector.dart';
+import 'package:brasil_cripto/l10n/global_app_localizations.dart';
 import 'package:brasil_cripto/view/utils/routes/app_navigator/app_navigator.dart';
 import 'package:brasil_cripto/view/utils/routes/main_routes.dart';
 import 'package:brasil_cripto/view/utils/widgets/buttons/default_button.dart';
 import 'package:brasil_cripto/view/utils/widgets/colors/ui_overlay_color.dart';
 import 'package:brasil_cripto/view/utils/widgets/images/asset_image_widget.dart';
+import 'package:brasil_cripto/view/utils/widgets/spacing/default_padding.dart';
 import 'package:brasil_cripto/view_model/utils/enums/app_images.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
@@ -19,6 +21,8 @@ class WelcomePage extends StatefulWidget {
 class _WelcomePageState extends State<WelcomePage> {
   double titleOpacity = 0;
   double subtitleOpacity = 0;
+
+  final localizations = sl<GlobalAppLocalizations>().current;
 
   @override
   void initState() {
@@ -38,41 +42,43 @@ class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.sizeOf(context);
-    final localizations = AppLocalizations.of(context)!;
 
     return UiOverlayColor(
       overlayColor: Colors.transparent,
       child: Scaffold(
-        body: Column(
-          children: [
-            AnimatedOpacity(
-              opacity: titleOpacity,
-              duration: Duration(milliseconds: 500),
-              child: AssetImageWidget(
-                image: AppImage.logo,
-                fit: BoxFit.contain,
-                size: Size(deviceSize.width, 500),
-              ),
-            ),
-            const Spacer(),
-            AnimatedOpacity(
-              duration: Duration(milliseconds: 500),
-              opacity: subtitleOpacity,
-              child: Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: deviceSize.width * 0.05,
-                ),
-                child: DefaultButton(
-                  label: localizations.start_label,
-                  onPressed: () {
-                    AppNavigator(
-                      context,
-                    ).pushReplaceNamed(MainRoutes.home.route);
-                  },
+        body: DefaultPadding(
+          includeHorizontal: false,
+          child: Column(
+            children: [
+              AnimatedOpacity(
+                opacity: titleOpacity,
+                duration: Duration(milliseconds: 500),
+                child: AssetImageWidget(
+                  image: AppImage.logo,
+                  fit: BoxFit.contain,
+                  size: Size(deviceSize.width, 500),
                 ),
               ),
-            ),
-          ],
+              const Spacer(),
+              AnimatedOpacity(
+                duration: Duration(milliseconds: 500),
+                opacity: subtitleOpacity,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: deviceSize.width * 0.05,
+                  ),
+                  child: DefaultButton(
+                    label: localizations.start_label,
+                    onPressed: () {
+                      AppNavigator(
+                        context,
+                      ).pushReplaceNamed(MainRoutes.home.route);
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
