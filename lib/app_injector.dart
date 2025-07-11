7cd59db4707ba18ef08f6de2ba5aa7e531d5bd77
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2025-07-11 18:11:42 -0300

    feat: add dependency injection setup in app_injector for service registration

---
+++ lib/app_injector.dart
@@ -0,0 +1,19 @@
+import 'package:brasil_cripto/l10n/global_app_localizations.dart';
+import 'package:brasil_cripto/model/repositories/home_repository.dart';
+import 'package:brasil_cripto/model/service/client/client_http.dart';
+import 'package:brasil_cripto/model/service/client/dio/dio_client.dart';
+import 'package:brasil_cripto/view_model/utils/secure_storage/secure_storage.dart';
+import 'package:brasil_cripto/view_model/utils/secure_storage/secure_storage_impl.dart';
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  sl.registerSingleton<SecureStorage>(SecureStorageImpl());
+
+  sl.registerSingleton<ClientHttp>(DioClient());
+
+  sl.registerSingleton<GlobalAppLocalizations>(GlobalAppLocalizationsImpl());
+
+  sl.registerFactory<HomeRepository>(() => HomeRepositoryImpl(client: sl()));
+}
