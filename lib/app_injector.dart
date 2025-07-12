6517fc8ed6e06e008dcd2986065cd8c72dc68553
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2025-07-11 21:45:50 -0300

    feat: register FavoritesService in app injector for dependency management

--- lib/app_injector.dart
+++ lib/app_injector.dart
@@ -2,6 +2,7 @@ import 'package:brasil_cripto/l10n/global_app_localizations.dart';
 import 'package:brasil_cripto/model/repositories/home_repository.dart';
 import 'package:brasil_cripto/model/service/client/client_http.dart';
 import 'package:brasil_cripto/model/service/client/dio/dio_client.dart';
+import 'package:brasil_cripto/view_model/services/favorites_service.dart';
 import 'package:brasil_cripto/view_model/utils/secure_storage/secure_storage.dart';
 import 'package:brasil_cripto/view_model/utils/secure_storage/secure_storage_impl.dart';
 import 'package:get_it/get_it.dart';
@@ -15,5 +16,9 @@ Future<void> init() async {
 
   sl.registerSingleton<GlobalAppLocalizations>(GlobalAppLocalizationsImpl());
 
-  sl.registerFactory<HomeRepository>(() => HomeRepositoryImpl(client: sl()));
+  sl.registerSingleton<FavoritesService>(FavoritesService());
+
+  sl.registerFactory<HomeRepository>(
+    () => HomeRepositoryImpl(client: sl(), secureStorage: sl()),
+  );
 }
